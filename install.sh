3a9b858408d88603b2b1151e4729e0d1546e897c
Max Ryabinin <mryabinin0@gmail.com>
2017-03-18 22:01:05 +0300

    Revert "force symlink"
    This reverts commit 4b64a43f6fc590b06d0b2c96a1466f0e8da344b7.

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # vim
-<<<<<<< HEAD
 ln -s ${BASEDIR}/.config/nvim/init.vim ~/.vimrc
 
 # bash
@@ -11,9 +10,3 @@ ln -s ${BASEDIR}/.bash_profile ~/.bashrc
 
 # git
 ln -s ${BASEDIR}/gitconfig ~/.gitconfig
-=======
-ln -sf ${BASEDIR}/.config/nvim/init.vim ~/.config/nvim/init.vim
-
-# zsh
-ln -sf ${BASEDIR}/.zshrc ~/.zshrc
->>>>>>> 2c1e01c... force symlink
